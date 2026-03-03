ae9c6516defd67e66e8f5df06c40e51c3d5b537a
NdaYoussef <ndayou99@gmail.com>
2026-03-03 09:40:45 +0200

    add schedule dto

---
+++ UniManagementSystem.Application/DTOs/ScheduleDtos/ScheduleStdDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniManagementSystem.Application.DTOs.ScheduleDto
+{
+    public class ScheduleStdDto
+    {
+        public string CourseName { get; set; }
+        public string Day { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Lecturer { get; set; }
+    }
+}
