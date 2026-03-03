2fed30aae7d095fc3061026ce3f4357d568c4061
NdaYoussef <ndayou99@gmail.com>
2026-03-03 09:39:45 +0200

    add std dto

---
+++ UniManagementSystem.Application/DTOs/DashboardDtos/StudentDtos/StudentDashboardDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.DTOs.DashboardDtos.StudentDtos;
+using UniManagementSystem.Application.DTOs.ScheduleDto;
+
+namespace UniManagementSystem.Application.DTOs.DashboardDtos
+{
+    public class StudentDashboardDto
+    {
+        public StudentInfoDto StudentInfo { get; set; } = new StudentInfoDto();
+        public int TotalCourses { get; set; }
+        public List<CourseDto> Courses { get; set; } = new List<CourseDto>();
+        public int UpcomingSchedules { get; set; }
+        public List<ScheduleStdDto> Schedules { get; set; } = new List<ScheduleStdDto>();
+    }
+}
---
+++ UniManagementSystem.Application/DTOs/DashboardDtos/StudentDtos/StudentInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniManagementSystem.Application.DTOs.DashboardDtos.StudentDtos
+{
+    public class StudentInfoDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public double GPA { get; set; }
+        public string ProfilePic { get; set; }
+    }
+}
