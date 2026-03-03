599a4f5be27d20ad1df141887395cab76c9aa080
NdaYoussef <ndayou99@gmail.com>
2026-03-03 09:39:16 +0200

    add course dto

---
+++ UniManagementSystem.Application/DTOs/CourseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace UniManagementSystem.Application.DTOs
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+
+        public int MaxDegree { get; set; } = 100;
+        public int MinDegree { get; set; } = 50;
+    }
+}
