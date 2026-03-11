45d37b313f44933b1d60a8b7ad51426fe58a011c
NdaYoussef <ndayou99@gmail.com>
2026-03-11 10:11:37 +0200

    add Base Class model for all classes

---
+++ UniManagementSystem.Domain/BaseModel/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniManagementSystem.Domain.BaseModel
+{
+    public class BaseClass<T>
+    {
+        public T Id { get; set; }   
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime CreatedBy { get; set; } 
+
+        public DateTime  UpdatedAt { get; set; } 
+        public DateTime  UpdatedBy { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
