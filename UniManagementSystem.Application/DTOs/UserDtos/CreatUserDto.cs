c25375a575c69b7766f2fd9774c49ccbe505ce0a
NdaYoussef <ndayou99@gmail.com>
2026-03-14 16:26:39 +0200

    add create new user dto

---
+++ UniManagementSystem.Application/DTOs/UserDtos/CreatUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniManagementSystem.Application.DTOs.UserDtos
+{
+    public class CreatUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public string? Address { get; set; }
+        public string NationalID { get; set; }
+        public double? GPA { get; set; }
+        public decimal? Salary { get; set; }
+        public int? DepartmentId { get; set; }
+        public string Role { get; set; } = "Student";
+    }
+}
