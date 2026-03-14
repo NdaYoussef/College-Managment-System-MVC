59c61fadae37b0224278bd25e1292cf40332931c
NdaYoussef <ndayou99@gmail.com>
2026-03-14 16:27:27 +0200

    add user info dto

---
+++ UniManagementSystem.Application/DTOs/UserDtos/UserInfoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniManagementSystem.Application.DTOs.UserDtos
+{
+    public class UserInfoDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; } = "N/A";
+        public string Role {  get; set; }
+    }
+}
