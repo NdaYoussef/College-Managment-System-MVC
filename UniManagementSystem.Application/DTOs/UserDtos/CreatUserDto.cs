668e4e500a8d5609c4883dd5936b75434e33f6b6
NdaYoussef <ndayou99@gmail.com>
2026-03-23 12:26:06 +0200

    remove user id from createUser Dto

--- UniManagementSystem.Application/DTOs/UserDtos/CreatUserDto.cs
+++ UniManagementSystem.Application/DTOs/UserDtos/CreatUserDto.cs
@@ -6,6 +6,7 @@ namespace UniManagementSystem.Application.DTOs.UserDtos
 {
     public class CreatUserDto
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
