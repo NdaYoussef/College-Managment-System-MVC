928516b3396189493ed16dbdf240b429fca7b709
NdaYoussef <ndayou99@gmail.com>
2026-03-23 21:30:20 +0200

    add user response dto

---
+++ UniManagementSystem.Application/DTOs/UserDtos/UserDetailsResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniManagementSystem.Application.DTOs.UserDtos
+{
+    public class UserDetailsResponseDto
+    {
+        public UserDashboardDto User { get; set; } 
+        public IList<string> Roles { get; set; }
+        public string DashboardRoute { get; set; }
+    }
+}
